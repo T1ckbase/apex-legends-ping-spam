44ee148cd9ded5219056ce4235ff42f241b04870
T1ckbase <146760065+T1ckbase@users.noreply.github.com>
2026-04-10 16:33:00 +0800

    changes

--- build.zig
+++ build.zig
@@ -2,14 +2,13 @@ const std = @import("std");
 
 pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
-
-    const optimize = b.standardOptimizeOption(.{ .preferred_optimize_mode = .ReleaseFast });
+    const optimize = std.builtin.OptimizeMode.ReleaseFast;
 
     const exe_mod = b.createModule(.{
         .root_source_file = b.path("src/main.zig"),
         .target = target,
         .optimize = optimize,
-        .strip = true,
+        .strip = optimize != .Debug,
     });
 
     const exe = b.addExecutable(.{
